62c139b766c0620987680d9249aeca88bfe6f5d9
dipshikha-01 <dipshikhajana2006@gmail.com>
2026-05-21 23:51:49 +0530

    Add files via upload

---
+++ pattern6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i , j ,a;
+    for (i=1 ; i<=4 ; i++)
+    {
+        for (a = 1 ;a<=i-1  ;a++)
+        {
+            printf(" ");
+        }
+            for(j = 1; j<=2*i-1 ;j++)
+        {
+            printf("*\t");                     
+        }
+        printf("\n");
+    }
+    return 0;
+}
