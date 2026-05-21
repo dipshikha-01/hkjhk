746e0108c46f7bcde2d5ba23697889e15a1aab74
dipshikha-01 <dipshikhajana2006@gmail.com>
2026-05-22 00:44:03 +0530

    Add files via upload

--- pattern7.c
+++ pattern7.c
@@ -8,7 +8,7 @@ int main()
         {
             printf(" ");
         }
-            for(j = 1; j<=i ;j++)
+            for(j = 1; j<=2*i-1 ;j++)
         {
             printf("*");                     
         }
